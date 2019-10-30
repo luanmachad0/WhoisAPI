f824df96df5f18f58688567025993e2c2e4b8599
luanmachad0 <42836089+luanmachad0@users.noreply.github.com>
2019-10-30 05:26:41 -0300

    first commit

---
+++ WebApplication2/Models/RequestApiWhois.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+
+
+namespace WebApplication2.Models
+{
+    public class RequestApiWhois
+    {
+        public readonly RestClient httpClient;
+        public RequestApiWhois()
+        {
+            httpClient = new RestClient("http://jsonwhoisapi.com");
+        }
+
+        public WhoisParsed SearchDomain(string dominio)
+        {
+            var request = new RestRequest("/api/v1/whois?identifier=" + dominio, Method.GET);
+            request.AddHeader("Authorization", "9XGXolqglOWnpM4_teQYVQ");
+
+            var response = httpClient.Execute(request);
+
+            var WhoisParsed = JsonConvert.DeserializeObject<WhoisParsed>(response.Content);
+
+            return WhoisParsed;
+        }
+    }
+}
---
+++ WebApplication2/Models/WhoisParsed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class WhoisParsed
+    {
+        public string name { get; set; }
+        public object created { get; set; }
+        public object changed { get; set; }
+        public object expires { get; set; }
+        public object dnssec { get; set; }
+        public object registered { get; set; }
+        public object status { get; set; }
+        public IList<object> nameservers { get; set; }
+        public object contacts { get; set; }
+        public object throttled { get; set; }
+
+    }
+}
